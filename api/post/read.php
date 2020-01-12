45154c6eb1d5412237b0899fc6d4d84c0a0d6ea4
mzwae <mrmonjaie@gmail.com>
2020-01-12 21:12:53 +0000

    updated read

--- api/post/read.php
+++ api/post/read.php
@@ -21,10 +21,25 @@
   if ($num > 0) {
     $posts_arr = array();
     $posts_arr['data'] = array();
-    while ($a <= 10) {
-      // code...
+    while ($row = $reult->fetch(PDO::FETCH_ASSOC)) {
+      extract($row);
+      $post_item = array(
+        'id' => $id,
+        'title' => $title,
+        'body' => html_entity_decode($body),
+        'author' => $author,
+        'category_id' => $category_id,
+        'category_name' => $category_name
+      );
+
+      array_push($posts_arr['data'], $post_item);
     }
-  } else {
+    // Turn into JSON and output
+    echo json_encode($posts_arr);
+  } else { // No posts
+    echo json_encode(
+      array('message' => 'No Posts Found')
+    );
 
   }
 
