719559dddf3dc2a180be9ad3cf81b9f7a67e0f5c
mzwae <mrmonjaie@gmail.com>
2020-01-14 21:26:31 +0000

    read single api

---
+++ api/post/read_single.php
@@ -0,0 +1,32 @@
+<?php
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Post.php';
+
+  // Intantiate DB and connect to DB
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $post = new Post($db);
+
+  // Get ID from URL
+  $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+  // Get post
+  $post->read_single();
+
+  // Create array
+  $post_arr = array(
+    'id' => $post->id,
+    'title' => $post->title,
+    'body' => $post->body,
+    'author' => $post->author,
+    'category_id' => $post->category_id,
+    'category_name' => $post->category_name
+  );
+
+  // Create JSON
+  print_r(json_encode($post_arr));
