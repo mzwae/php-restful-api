d5422ebfd924e9a12d1e7577a8900a950dbfd09d
mzwae <mrmonjaie@gmail.com>
2020-01-22 21:59:51 +0000

    Added delete ctrl

--- api/post/delete.php
+++ api/post/delete.php
@@ -2,7 +2,7 @@
 // Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: UPDATE');
+header('Access-Control-Allow-Methods: DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/Database.php';
@@ -21,20 +21,15 @@ $data = json_decode(file_get_contents("php://input"));
 // Set ID  to update
 $post->id = $data->id;
 
-$post->title = $data->title;
-$post->body = $data->body;
-$post->author = $data->author;
-$post->category_id = $data->category_id;
+// Delete post
 
-// Update post
-
-if ($post->update()) {
+if ($post->delete()) {
   echo json_encode(
-    array('message' => 'Post Updated')
+    array('message' => 'Post Deleted')
   );
 } else {
   echo json_encode(
-    array('message' => 'Post Not Updated')
+    array('message' => 'Post Not Deleted')
   );
 }
 
