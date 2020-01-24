27c0b4f3b17d2ad2d099c2a17648e11fab85f9e2
mzwae <mrmonjaie@gmail.com>
2020-01-24 23:22:04 +0000

    updated read category model

--- models/Categories.php
+++ models/Categories.php
@@ -20,6 +20,19 @@ public function __construct($db){
 // Get categories
 public function read(){
   // Create query
+  $query = 'SELECT
+    id, name
+  FROM
+    ' . $this->table . '
+  ORDER BY created_at DESC';
+
+  // Prepare statement
+  $stmt = $this->conn->prepare($query);
+
+  // Execute query
+  $stmt->execute();
+
+  return $stmt;
 }
 }
 
