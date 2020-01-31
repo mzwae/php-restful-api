b85a134f0720e8ba9e81e34460c9e1872a3a0900
mzwae <mrmonjaie@gmail.com>
2020-01-31 21:35:23 +0000

    added create category model

--- models/Category.php
+++ models/Category.php
@@ -4,40 +4,43 @@
  */
 class Category
 {
-private $conn;
-private $table = 'categories';
+    private $conn;
+    private $table = 'categories';
 
-// Properties
-public $id;
-public $name;
-public $created_at;
+    // Properties
+    public $id;
+    public $name;
+    public $created_at;
 
-// Constructor with DB
-public function __construct($db){
-  $this->conn = $db;
-}
+    // Constructor with DB
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 
-// Get categories
-public function read(){
-  // Create query
-  $query = 'SELECT
+    // Get categories
+    public function read()
+    {
+        // Create query
+        $query = 'SELECT
     id, name, created_at
   FROM
     ' . $this->table . '
   ORDER BY created_at DESC';
 
-  // Prepare statement
-  $stmt = $this->conn->prepare($query);
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
 
-  // Execute query
-  $stmt->execute();
+        // Execute query
+        $stmt->execute();
 
-  return $stmt;
-}
+        return $stmt;
+    }
 
-// Get single category
-public function read_single(){
-  $query = 'SELECT
+    // Get single category
+    public function read_single()
+    {
+        $query = 'SELECT
   id, name, created_at
 FROM
   ' . $this->table . ' p
@@ -45,23 +48,48 @@ WHERE
   p.id = ?
 LIMIT 0,1';
 
-// Prepare statement
-$stmt = $this->conn->prepare($query);
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
 
-// Bind ID
-$stmt->bindParam(1, $this->id);
+        // Bind ID
+        $stmt->bindParam(1, $this->id);
 
-// Execute query
-$stmt->execute();
+        // Execute query
+        $stmt->execute();
 
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// Set properties
-$this->id = $row['id'];
-$this->name = $row['name'];
-$this->created_at = $row['created_at'];
+        // Set properties
+        $this->id = $row['id'];
+        $this->name = $row['name'];
+        $this->created_at = $row['created_at'];
+    }
+
+    // Create Post
+    public function create()
+    {
+        // Create query
+        $query = 'INSERT INTO ' .
+        $this->table . '
+    SET
+      name = :name';
+
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        // Sanitize data
+        $this->name = htmlspecialchars(strip_tags($this->name));
+
+        // Bind data
+        $stmt->bindParam(':name', $this->name);
+
+        // Execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        // Print error if something goes wrong
+        printf("Error: %s. \n", $stmt->error);
+        return false;
+    }
 }
-}
-
-
- ?>
