3e22116e559e05cb8880f51945c30026d8d130e2
mzwae <mrmonjaie@gmail.com>
2020-02-01 21:37:17 +0000

    added update cat model

--- models/Category.php
+++ models/Category.php
@@ -65,7 +65,7 @@ LIMIT 0,1';
         $this->created_at = $row['created_at'];
     }
 
-    // Create Post
+    // Create a new category
     public function create()
     {
         // Create query
@@ -91,5 +91,38 @@ LIMIT 0,1';
         // Print error if something goes wrong
         printf("Error: %s. \n", $stmt->error);
         return false;
+    }
+
+
+    // Update Post
+    public function update(){
+      // Create query
+      $query = 'UPDATE ' .
+        $this->table . '
+        SET
+          name = :name
+        WHERE
+          id = :id';
+
+          // Prepare statement
+          $stmt = $this->conn->prepare($query);
+
+          // Sanitize data
+          $this->name = htmlspecialchars(strip_tags($this->name));
+          $this->id = htmlspecialchars(strip_tags($this->id));
+
+          // Bind data
+          $stmt->bindParam(':name', $this->name);
+          $stmt->bindParam(':id', $this->id);
+
+          // Execute query
+          if ($stmt->execute()) {
+            return true;
+          }
+
+          // Print error if something goes wrong
+          printf("Error: %s. \n", $stmt->error);
+          return false;
+
     }
 }
