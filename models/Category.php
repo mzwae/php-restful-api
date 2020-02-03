a1ca4952554b1d416f406ffc3695e9a28b94fe2c
mzwae <mrmonjaie@gmail.com>
2020-02-03 21:19:39 +0000

    added delete category model

--- models/Category.php
+++ models/Category.php
@@ -125,4 +125,29 @@ class Category
           return false;
 
     }
+
+    // Delete Category
+    public function delete(){
+      // Create query
+      $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Clean data
+      $this->id = htmlspecialchars(strip_tags($this->id));
+
+      // Bind data
+      $stmt->bindParam(':id', $this->id);
+
+      // Execute query
+      if ($stmt->execute()) {
+        return true;
+      }
+
+      // Print error if something goes wrong
+      printf("Error: %s.\n", $stmt->error);
+
+      return false;
+    }
 }
